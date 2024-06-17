8d1666d9d43a85f44dfd8f4a90a34e8d1af7258c
Robel Shewangzaw <robel@addissoftware.com>
2024-06-17 22:56:34 +0300

    setup the continous integration and contionous deployment

---
+++ src/users/users.controller.ts
@@ -0,0 +1,18 @@
+import { Controller, Get, Param, Delete } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { User } from './user.entity';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Get()
+  findAll(): Promise<User[]> {
+    return this.usersService.findAll();
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string): Promise<void> {
+    return this.usersService.remove(id);
+  }
+}
