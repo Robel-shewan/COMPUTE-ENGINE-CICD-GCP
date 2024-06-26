91b423196f49755a6b2611a89cd123039ed90cfb
Robel Shewangzaw <robel@addissoftware.com>
2024-06-26 05:07:08 +0300

    testing CICD

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,3 +1,26 @@
+// import { Module } from '@nestjs/common';
+// import { TypeOrmModule } from '@nestjs/typeorm';
+// import { AppController } from './app.controller';
+// import { AppService } from './app.service';
+
+// @Module({
+//   imports: [
+//     TypeOrmModule.forRoot({
+//       type: 'mysql',
+//       host: process.env.DB_HOST,
+//       port: parseInt(process.env.DB_PORT, 10),
+//       username: process.env.DB_USERNAME,
+//       password: process.env.DB_PASSWORD,
+//       database: process.env.DB_NAME,
+//       entities: [__dirname + '/**/*.entity{.ts,.js}'],
+//       synchronize: true,
+//     }),
+//   ],
+//   controllers: [AppController],
+//   providers: [AppService],
+// })
+// export class AppModule {}
+
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
@@ -7,11 +30,11 @@ import { AppService } from './app.service';
   imports: [
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_PORT, 10),
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
+      host: '127.0.0.1',
+      port: 3306,
+      username: 'root',
+      password: 'Root123#',
+      database: 'mydb',
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
     }),
