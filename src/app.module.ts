57cb3cca0a8817fa56787e3615c0971fc35d5e6f
Robel Shewangzaw <robel@addissoftware.com>
2024-06-26 06:42:38 +0300

    latest update with statics environment

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,26 +1,3 @@
-import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-
-@Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_PORT, 10),
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: true,
-    }),
-  ],
-  controllers: [AppController],
-  providers: [AppService],
-})
-export class AppModule {}
-
 // import { Module } from '@nestjs/common';
 // import { TypeOrmModule } from '@nestjs/typeorm';
 // import { AppController } from './app.controller';
@@ -30,11 +7,11 @@ export class AppModule {}
 //   imports: [
 //     TypeOrmModule.forRoot({
 //       type: 'mysql',
-//       host: '127.0.0.1',
-//       port: 3306,
-//       username: 'root',
-//       password: 'Root123#',
-//       database: 'mydb',
+//       host: process.env.DB_HOST,
+//       port: parseInt(process.env.DB_PORT, 10),
+//       username: process.env.DB_USERNAME,
+//       password: process.env.DB_PASSWORD,
+//       database: process.env.DB_NAME,
 //       entities: [__dirname + '/**/*.entity{.ts,.js}'],
 //       synchronize: true,
 //     }),
@@ -43,3 +20,26 @@ export class AppModule {}
 //   providers: [AppService],
 // })
 // export class AppModule {}
+
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: '127.0.0.1',
+      port: 3306,
+      username: 'root',
+      password: 'Root123#',
+      database: 'mydb',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
